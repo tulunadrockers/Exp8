90bde497e3e9ccb99f69ea5d63e35add7fc1b309
tulunadrockers <tulunadrockers2@gmail.com>
2026-04-27 21:27:10 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import ReminderApp from './ReminderApp'; 
+ 
+function App() { 
+    return ( 
+        <div className="App"> 
+            <ReminderApp /> 
+        </div> 
+    ); 
+} 
+ 
+export default App; 
